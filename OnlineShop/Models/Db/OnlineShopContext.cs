917d54cc2f5a0a68de1d9d02f4434d39bd3a7a89
mosttoffa <mostofacba@gmail.com>
2025-07-10 10:57:20 +0600

    Add project files.

---
+++ OnlineShop/Models/Db/OnlineShopContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Models.Db;
+
+public partial class OnlineShopContext : DbContext
+{
+    public OnlineShopContext()
+    {
+    }
+
+    public OnlineShopContext(DbContextOptions<OnlineShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MOSTOFA\\SQLEXPRESS01;Database=OnlineShop;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Link).HasMaxLength(350);
+            entity.Property(e => e.MenuTitle).HasMaxLength(100);
+            entity.Property(e => e.Type).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ OnlineShop/Program.cs
@@ -0,0 +1,31 @@
+using OnlineShop.Models.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<OnlineShopContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
