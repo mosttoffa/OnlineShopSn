bcf52d3d16e6a0d1453b58784572d0d3439c793c
Mostofa <mostofacba@gmail.com>
2025-07-22 22:34:28 +0600

    Create Products Controller

---
+++ OnlineShop/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models.Db;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly OnlineShopContext _context;
+
+        public ProductsController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.OrderByDescending(x=> x.Id).ToList();
+            return View(products);
+        }
+    }
+}
